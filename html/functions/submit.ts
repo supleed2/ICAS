fe57b5b06fd68d22256c0d8edcff8d3f802a2f7a
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-09-08 01:00:29 +0100

    Dynamic origin in submission redirect URLs

--- html/functions/submit.ts
+++ html/functions/submit.ts
@@ -1,17 +1,19 @@
 export async function onRequestPost({ request, env }) {
-	let { username, password, discordID } = Object.fromEntries(await request.formData().then((f) => f.entries()));
+	const origin = new URL(request.url).origin;
+	const { username, password, discordID } = Object.fromEntries(await request.formData().then((f) => f.entries()));
 
-	let login = await fetch("https://eactivities.union.ic.ac.uk/user/login", {
+	const login = await fetch("https://eactivities.union.ic.ac.uk/user/login", {
 		method: "POST",
 		body: JSON.stringify({ username: username, password: password, }),
 		headers: { "content-type": "application/json", },
 	}).catch(console.error);
 
 	if (login == undefined || login.status != 200) {
-		console.log(`Login verification failed 😢 for discord user [${discordID}] with shortcode [${username}]`);
-		return Response.redirect("http://127.0.0.1:8788/verify/failure", 301);
+		console.log(`😢 Login verification failed for discord user [${discordID}] with shortcode [${username}]`);
+		return Response.redirect(`${origin}/verify/failure`, 301);
 	} else {
-		console.log(`Login verification succeeded 🚀 for discord user [${discordID}] with shortcode [${username}]`);
-		return Response.redirect("http://127.0.0.1:8788/verify/success", 301);
+		console.log(`🚀 Login verification succeeded for discord user [${discordID}] with shortcode [${username}]`);
+		// TODO: Send username and discordID to Nanobot
+		return Response.redirect(`${origin}/verify/success`, 301);
 	}
 }
